f543fd7761ca844f310b1e4d263e2eb582911966
KoreanWaffles <77174590+KoreanWaffles@users.noreply.github.com>
2021-01-11 23:22:11 -0600

    Added Klaus Figure Sketch to loot

--- scripts/prefabs/klaus.lua
+++ scripts/prefabs/klaus.lua
@@ -13,6 +13,7 @@ local prefabs =
     "deer_red",
     "deer_blue",
     "staff_castinglight",
+    "chesspiece_klaus_sketch",
 
     --winter loot
     "winter_food3", --Candy Cane
@@ -22,7 +23,9 @@ local loot =
 {
     "monstermeat",
     "charcoal",
+    "chesspiece_klaus_sketch",
 }
+
 
 --------------------------------------------------------------------------
 
