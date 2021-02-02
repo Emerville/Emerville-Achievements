89e1448bf575d87186d870b5a294a1d8dc236c2e
NSavard <digzol@users.noreply.github.com>
2021-02-02 08:48:22 -0500

    Cleanup spaces & indentation

--- scripts/diningtableaction.lua
+++ scripts/diningtableaction.lua
@@ -4,25 +4,22 @@ local ActionHandler = GLOBAL.ActionHandler
 
 local DINE = GLOBAL.Action({ mount_valid = true })
 DINE.str = "Dine"
-DINE.id = "DINE" 
-DINE.fn = function(act)  
-	if act.doer and act.target and act.target.components.dining and act.target.foodname then   
-		act.doer.components.eater:Eat(act.target.components.dining:GetFood(), act.doer)   
-	    return true  
-    end  
-	return false 
-end 
+DINE.id = "DINE"
+DINE.fn = function(act)
+    if act.doer and act.target and act.target.components.dining and act.target.foodname then
+        act.doer.components.eater:Eat(act.target.components.dining:GetFood(), act.doer)
+        return true
+    end
+    return false
+end
 
-AddAction(DINE) 
+AddAction(DINE)
 
-AddComponentAction("SCENE", "dining", function(inst, doer, actions, right)  
+AddComponentAction("SCENE", "dining", function(inst, doer, actions, right)
+    if right then
+        table.insert(actions, ACTIONS.DINE)
+    end
+end)
 
-	if right then   
-		table.insert(actions, ACTIONS.DINE)  
-	end 
-end) 
-AddStategraphActionHandler("wilson", ActionHandler(ACTIONS.DINE, "doshortaction")) 
+AddStategraphActionHandler("wilson", ActionHandler(ACTIONS.DINE, "doshortaction"))
 AddStategraphActionHandler("wilson_client", ActionHandler(ACTIONS.DINE, "doshortaction"))
-
---"eat"
-  
