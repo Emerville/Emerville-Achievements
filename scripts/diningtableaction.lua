4bbcb4bd3652b846a22a4be524f48cc25b23b43b
KoreanWaffles <77174590+KoreanWaffles@users.noreply.github.com>
2022-02-04 23:59:34 -0500

    Added ownership to dining action.

--- scripts/diningtableaction.lua
+++ scripts/diningtableaction.lua
@@ -7,6 +7,13 @@ DINE.str = "Dine"
 DINE.id = "DINE"
 DINE.fn = function(act)
     if act.doer and act.target and act.target.components.dining then
+        if act.target:HasTag("uid_private") then
+            if act.doer.userid and act.target:HasTag("uid_"..act.doer.userid)
+                or act.doer.Network and act.doer.Network:IsServerAdmin() then
+                return act.target.components.dining:Dine(act.doer)
+            end
+            return false
+        end
         return act.target.components.dining:Dine(act.doer)
     end
 end
